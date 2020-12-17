f59c3c781eb2ee96d8081e7fe15ef40cf629d8c2
syrawat <72315396+syrawat@users.noreply.github.com>
2020-12-17 20:25:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,106 @@
+
+var database ,dog,dog1,dog2
+var position
+//var form
+var feed,add
+var foodobject
+var Feedtime
+var Lastfeed
+//Create variables here
+
+function preload()
+
+{
+  dogimg1 = loadImage("sitting.jpg")
+  dogimg2 = loadImage("drinking.jpg")
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 500);
+  database = firebase.database();
+  console.log(database);
+ 
+  foodobject=new Food();
+  dog = createSprite(550,250,10,10);
+  dog.addImage(dogimg1)
+  dog.scale=0.2
+ 
+  var dogo = database.ref('Food');
+  dogo.on("value", readPosition, showError);
+  feed = createButton("FEED DOG")
+  feed.position(900,60)
+  feed.mousePressed(FeedDog)
+
+  add = createButton("ADD FOOD")
+  add.position(800,60)
+  add.mousePressed(AddFood)
+
+} 
+
+function draw(){
+ background(46,139,87);
+ fill(255,255,254);
+  textSize(15); 
+  if(Lastfeed>=12){ 
+    text("Last Feed : "+ Lastfeed%12 + " PM", 200,30); }
+  else if(Lastfeed==0){ 
+    text("Last Feed : 12 AM",200,30); }
+    else{ 
+      text("Last Feed : "+ Lastfeed + " AM", 200,30); }
+ foodobject.display()
+ 
+ 
+  
+ fill(255,255,254);
+ textSize(15);
+
+drawSprites();
+}
+function readPosition(data){
+  position = data.val();
+  foodobject.updateFoodStock(position)
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
+
+function writePosition(x){
+  if(x>0){
+    x=x-1
+  }
+  else{
+    x=0
+  }
+  database.ref('/').set({
+    'Food': x
+  })
+
+}
+function AddFood(){
+position++
+database.ref('/').update({
+  Food:position
+}
+
+)
+}
+function FeedDog(){
+if(this.foodStock !== 0){
+dog.addImage(dogimg2)
+dog.scale = 0.9
+foodobject.updateFoodStock(foodobject.getFoodStock()-1)
+ database.ref('/').update({
+   Food:foodobject.getFoodStock(),
+   FeedTime:hour ()
+ })
+}else{
+  dog.addImage(dogimg1)
+}
+}
+
+
+
+
+
